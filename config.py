fb7ef852ebd69b1c7bcaf5425f1ececc57aedd43
Aublic-sudo <rstock937@gmail.com>
2025-04-14 17:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8096773919:aafX1ncdalNPIQuIZUa0d-96lFrVtoVP3pO")
+    API_ID = int(os.environ.get("API_ID", "25352517"))
+    API_HASH = os.environ.get("API_HASH", "2b7e6cf7752c3af91f958d67793a3e0b")
+    AUTH_USERS = "7360968885"
 

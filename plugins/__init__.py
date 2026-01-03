2b364fb59d237bd285163ba416f87b5b9d6c42d1
Aublic-sudo <rstock937@gmail.com>
2026-01-03 13:16:12 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -32,14 +32,16 @@ async def Start_msg(bot: stark , m: Message):
            
 
 
-@stark.on_message(filters.command(["restart"]) 
+@stark.on_message(filters.command(["restart"]))
 async def restart_handler(_, m):
     await m.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-@stark.on_message(filters.command(["log"]) 
+
+@stark.on_message(filters.command(["log"]))
 async def log_msg(bot: stark , m: Message):   
     await bot.send_document(m.chat.id, "log.txt")
+
 
 
 
